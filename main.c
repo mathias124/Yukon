775ee3229a3a3a21abd5391159bbdd4394646417
mathias124 <mfs2001@hotmail.dk>
2023-04-14 11:10:44 +0200

    Update, LoadFile & TxtFile Added.

--- main.c
+++ main.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *file_ptr;
+
+    char *buffer;
+    long file_size;
+    // Open the file
+    file_ptr = fopen("C:\\Users\\Mathias\\CLionProjects\\untitled1\\DATA.txt", "r");
+
+    // Get the file size
+    fseek(file_ptr, 0, SEEK_END);
+    file_size = ftell(file_ptr);
+    rewind(file_ptr);
+
+    // Allocate memory for the file buffer
+    buffer = (char*) malloc(file_size * sizeof(char));
+    if (!buffer) {
+        printf("Error: Could not allocate memory for file buffer\n");
+        fclose(file_ptr);
+        return 1;
+    }
+
+    // Read the file into the buffer
+    fread(buffer, sizeof(char), file_size, file_ptr);
+
+    // Close the file
+    fclose(file_ptr);
+
+    // Print the contents of the file
+    printf("%s", buffer);
+
+    // Free the buffer memory
+    free(buffer);
+
     return 0;
 }
