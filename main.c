2293f156c61af9120ef898befd12e7b76387b50b
mathias124 <mfs2001@hotmail.dk>
2023-04-14 12:54:11 +0200

    Update.

--- main.c
+++ main.c
@@ -1,35 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main() {
     FILE *file_ptr;
 
+    char cwd[PATH_MAX];
     char *memoryBuff;
     long file_size;
+
+
+    //char *FileName = "cmake-build-debug/DATA.txt";
     // Open the file
-    file_ptr = fopen("C:\\Users\\Mathias\\CLionProjects\\untitled1\\DATA.txt", "r");
-
-    // Get the file size
-    fseek(file_ptr, 0, SEEK_END);
-    file_size = ftell(file_ptr);
-    rewind(file_ptr);
-
-    // Allocate memory for the file buffer
-    memoryBuff = (char*) malloc(file_size * sizeof(char));
-    if (!memoryBuff) {
-        printf("Error: Could not allocate memory for file buffer\n");
-        fclose(file_ptr);
-        return 1;
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+        perror("getcwd() error");
+        exit(EXIT_FAILURE);
     }
-    // Read the file into the buffer
-    fread(memoryBuff, sizeof(char), file_size, file_ptr);
-    // Close the file
-    fclose(file_ptr);
-    // Print the contents of the file
-    printf("%s", memoryBuff);
+       // printf("Current working directory: %s\n", cwd);
+    char* file_name = "DATA.txt";
+    char file_path[PATH_MAX];
+    snprintf(file_path, sizeof(file_path), "%s/%s", cwd, file_name);
 
-    // Free the buffer memory
-    free(memoryBuff);
+    printf("Current working directory: %s\n", cwd);
+    printf("File path: %s\n", file_path);
 
-    return 0;
-}
+        file_ptr = fopen(file_path, "r");
+        //file_ptr = fopen(FileName, "r");
+        // Get the file size
+        fseek(file_ptr, 0, SEEK_END);
+        file_size = ftell(file_ptr);
+        rewind(file_ptr);
+
+        // Allocate memory for the file buffer
+        memoryBuff = (char *) malloc(file_size * sizeof(char));
+        if (!memoryBuff) {
+            printf("Error: Could not allocate memory for file buffer\n");
+            fclose(file_ptr);
+            return 1;
+        }
+        // Read the file into the buffer
+        fread(memoryBuff, sizeof(char), file_size, file_ptr);
+        // Close the file
+        fclose(file_ptr);
+        // Print the contents of the file
+        printf("%s", memoryBuff);
+
+        // Free the buffer memory
+        free(memoryBuff);
+
+        return 0;
+    }
+
