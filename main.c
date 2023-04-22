d092baa4e776d78b35bd10d7c5c321bb0e6f782d
mathias124 <mfs2001@hotmail.dk>
2023-04-22 13:00:38 +0200

    Commit, with lines, but can be fixed.

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main() {
     char cwd[1024];
     char *memoryBuff;
     long file_size;
-    int SizeOfCards =13;
+    int SizeOfCards = 13;
     //char c = (char) "c";
     char clubs['C'];
     char Hearts['H'];
@@ -18,21 +18,20 @@ int main() {
     char Spades['S'];
     int count = 0;
     char VariableT[2];
-    int DiamondArray = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+    char VariableC[2];
+    int DiamondArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    char ColorSize[13];
+    char Array[0];
 
 
-
-   typedef struct CardType {
-     char  name[2];
-  struct CardType * next;
-    //Max 2
+    typedef struct CardType {
+        char name[2];
+        struct CardType *next;
+        //Max 2
 
     };
 
     struct CardType cards[52];
-
-
-
 
     //char *FileName = "cmake-build-debug/DATA.txt";
     // Open the file, using CWD library to get a user's directory path to make it work.
@@ -40,63 +39,86 @@ int main() {
         perror("getcwd() error");
         exit(EXIT_FAILURE);
     }
-    char* file_name = "DATA.txt";
+    char *file_name = "DATA.txt";
     char file_path[1024];
     snprintf(file_path, sizeof(file_path), "%s/%s", cwd, file_name);
-
     printf("File path: %s\n", file_path);
+    file = fopen(file_path, "r");
+    fseek(file, 0, SEEK_END);
+    file_size = ftell(file);// Get the file size
+    rewind(file);
 
-        file = fopen(file_path, "r");
 
-        fseek(file, 0, SEEK_END);
-        file_size = ftell(file);// Get the file size
-        rewind(file);
 
-        // Allocate memory for the file buffer
-        memoryBuff = (char *) malloc(file_size * sizeof(char));
-        if (!memoryBuff) {
-            printf("Error: Could not allocate memory for file buffer\n");
-            fclose(file);
-            return 1;
+
+    // Allocate memory for the file buffer
+    memoryBuff = (char *) malloc(file_size * sizeof(char));
+    if (!memoryBuff) {
+        printf("Error: Could not allocate memory for file buffer\n");
+        fclose(file);
+        return 1;
+    }
+
+
+    while (!feof(file)) {
+        int res = fscanf(file, "%c%c", VariableC, VariableT);
+        if (res == 2) {
+            for(int i = 0; i<file_size; i++) {
+                if (memoryBuff[i] != "\n"){
+
+                }
+            }
+
+            printf("VariableChar : %s\n",VariableC);
+            printf("VariableT : %s\n",VariableT);
         }
-        // Read the file into the buffer
-        fread(memoryBuff, sizeof(char), file_size, file);
-        // Close the file
+}
+
+
+
+    // Read the file into the buffer
+    fread(memoryBuff, sizeof(char), file_size, file);
+    // Close the file
     fclose(file);
-        // Print the contents of the file
-        for(int i=0;i<file_size;i++){
-            if(memoryBuff[i]!="\n")
-             //   cards[i]=
-            printf("%c", memoryBuff[i]);
-        }
+    // Print the contents of the file
+    //for (int i = 0; i < file_size; i += 2) {
+        //if (memoryBuff[i] != "\n" && memoryBuff[i + 1] != "\n") {
+            //Array[count++] = memoryBuff[i];
+            //Array[count++] = memoryBuff[i + 1];
+           // printf("%c",Array[count]);
 
 
 
 
-        // Free the buffer memory
+
+            //printf("%c%c", Array[j], Array[j + 1]);
+
+            //printf("%c", memoryBuff[i]);
+            // Free the buffer memory
+
+
         free(memoryBuff);
 
 
+        /* struct CardType * createCard(char name [2], int pointer) {
+            Cardt *newFlight;
+    // allocate a new node
+            newFlight = (Flight*)malloc(sizeof(Flight));
+    // initialize node data
+            strcpy(newFlight->ID, ID);
+            newFlight->longitude = longitude;
+            newFlight->latitude = latitude;
+            newFlight->altitude = altitude;
+            newFlight->airSpeed = airSpeed;
+    // initialize pointer
+            newFlight->next = NULL;
+            return newFlight;
+        }
 
-    /* struct CardType * createCard(char name [2], int pointer) {
-        Cardt *newFlight;
-// allocate a new node
-        newFlight = (Flight*)malloc(sizeof(Flight));
-// initialize node data
-        strcpy(newFlight->ID, ID);
-        newFlight->longitude = longitude;
-        newFlight->latitude = latitude;
-        newFlight->altitude = altitude;
-        newFlight->airSpeed = airSpeed;
-// initialize pointer
-        newFlight->next = NULL;
-        return newFlight;
-    }
-
-    */
-
+        */
 
 
-    return 0;
+
+        return 0;
     }
 
