339163c0258802857600ecf34d1b42e7d5cc5dd5
Marble <62856659+Marble5@users.noreply.github.com>
2023-04-25 17:03:24 +0200

    Cleanup

--- main.c
+++ main.c
@@ -10,18 +10,9 @@ int main() {
     char cwd[1024];
     char *memoryBuff;
     long file_size;
-    int SizeOfCards = 13;
-    //char c = (char) "c";
-    char clubs['C'];
-    char Hearts['H'];
-    char Diamonds['D'];
-    char Spades['S'];
-    int count = 0;
+
     char cardColor[2];
     char cardValue[2];
-    int DiamondArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
-    char ColorSize[13];
-    char Array[0];
 
 
     struct CardType {
@@ -93,10 +84,6 @@ int main() {
     fread(memoryBuff, sizeof(char), file_size, file);
     // Close the file
     fclose(file);
-    // Print the contents of the file
-    //for (int i = 0; i < file_size; i += 2) {
-    //printf("%c", memoryBuff[i]);
-    // Free the buffer memory
     free(memoryBuff);
     return 0;
 }
