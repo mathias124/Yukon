79b5e82c9cba7df9e5a2f0227ffb348f6a3a7d0f
Felix Schmidt <s224313@dtu.dk>
2023-04-27 18:55:19 +0200

    Cards now consist of values and suits instead of names for easy decleration. First view of 7 columns.

--- readFile.h
+++ readFile.h
@@ -1,6 +1,9 @@
 #ifndef READFILE_H
 #define READFILE_H
 
+#include <stdio.h>
+#include "card.h"
+
 FILE * file;
 
 char cwd[1024];
@@ -10,4 +13,9 @@ long file_size;
 char *file_name = "DATA.txt";
 char file_path[1024];
 
+typedef struct ReadFile{
+    Card card;
+}ReadFile;
+
+ReadFile readFromFile(char *file);
 #endif
