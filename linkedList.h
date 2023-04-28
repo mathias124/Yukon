8e96e6561fe8f953ed7b2cf096b0775a35c94693
Abdisamad Nuh <abdisamad.nuh@outlook.com>
2023-04-28 10:53:47 +0200

    .h linkedlist initial variables and methods

---
+++ linkedList.h
@@ -0,0 +1,22 @@
+//
+// Created by Abdis on 4/28/2023.
+//
+
+#ifndef UNTITLED1_LINKEDLIST_H
+#define UNTITLED1_LINKEDLIST_H
+#include "card.h"
+#include <stdio.h>
+typedef struct Node {
+    char name;
+    struct node * next;
+};
+
+void insert(Card *card,struct Node * node){
+
+
+}
+void remove(Card *card,struct Node * node)
+
+
+
+#endif //UNTITLED1_LINKEDLIST_H
